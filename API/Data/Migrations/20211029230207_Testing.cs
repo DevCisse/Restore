3388bcd98f423ff6ac800411184695a080d32e63
Hassan Salihu <45653783+DevCisse@users.noreply.github.com>
2021-10-30 12:20:17 +0100

    end of section 2

---
+++ API/Data/Migrations/20211029230207_Testing.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class Testing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Test",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Test",
+                table: "Products");
+        }
+    }
+}
